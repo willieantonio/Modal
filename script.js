9fa15c64246777a94a7dfa6503ed8ef819727876
Willie Antonio <willie.antonio.santos@gmail.com>
2021-02-24 11:40:41 -0300

    Adicionando mais Javascript ao codigo

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-function require(infoDev){
-    if(infoDev.name.value === "" ||infoDev.name.value === null ||
-     infoDev.name.lenght < 8){
-        alert("Preencha o campo Nome")
-        infoDev.name.focus();
+
+//Verificação dos campos do Formulario
+function require(infoDev) {
+    if (infoDev.name.value === "" || infoDev.name.value === null ||
+        infoDev.name.lenght < 8) {
+        alert("Preencha o campo 'Nome'")
+        infoDev.name.focus(); //Paradeixar o campo destacado
         return false;
         //Verificação do nome digitado
     }
-    if(infoDev.name.value.indexOf("@") == -1||
-       infoDev.name.value.indexOf(".") == -1||
-       infoDev.email.value === "" ||
-       infoDev.email.value == null){
-        alert("Preencha o campo E-mail")
-        infoDev.name.focus();
+    if (infoDev.email.value.indexOf("@") == -1 ||
+        infoDev.email.value.indexOf(".") == -1 ||
+        infoDev.email.value === "" ||
+        infoDev.email.value == null) {
+        alert("Preencha o campo 'E-mail'")
+        infoDev.name.focus();//Paradeixar o campo destacado
         return false;
         //Verificação do E-mail
     }
-    if(job === null){
-        alert("Escolha um Item")
+    
+    if (infoDev.job.value === "") {
+        alert("Escolha uma 'Atividade'")
         return false;
+        infoDev.job.focus();//Paradeixar o campo destacado
         //Verificação do job
     }
+    return true; 
+   
+}
+//Para recarregar a pagina 
+function recharge() {
+    window.location.reload();
+    return true;
 }
 
 
-//Função para enviar os dados
-function Enviar() {    
-        alert("Dados enviados com sucesso")
-        return true;
-}
+
+
+
+
 
